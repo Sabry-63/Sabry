f40a05524d0e3ac9df3cef57bfa158785765fb2d
Sabry-63 <52372174+Sabry-63@users.noreply.github.com>
2020-04-01 14:51:05 +0200

    Add files via upload

--- js/min.js
+++ js/min.js
@@ -1,52 +1,75 @@
-/*global $, console */
-$(function() {
+/*global $, console, WOW */
+$(function () {
     "use strict";
-    const widthNav = $(".navbar").innerWidth();
+  // Navbar function
+  // Width Auto Nav
+    function widthNavbar() {
+        let WidthN = $(".navbar").innerWidth();
+        return WidthN;
+    }
+    let widthNav = widthNavbar();
 
-    // WOW ANIMATION
-    const wow = new WOW();
-    wow.init();
+  // Show Auto Nav
+    function showNavbar() {
+        const ShowNavbar = $(".content-nav").animate({
+            width: 100 + "%"
+        }, 600);
+        $(".navbar").animate({
+            left: 0
+        }, 600);
+        return ShowNavbar;
+    }
+  // Closed Auto Nav
+    function closedNavbar() {
+        const ClosedNavbar = $(".navbar").animate({
+            left: -widthNav
+        }, 600);
+        $(".content-nav").animate({
+            width: 0
+        }, 600);
+        $(".navbar").removeClass("show-list");
+        return ClosedNavbar;
+    }
 
-    // PRELOADER
-    $(window).load(function() {
-        $("#preloader").fadeOut("slow", function() {
+  // WOW ANIMATION
+    const Wow = new WOW();
+    Wow.init();
+
+  // PRELOADER
+    $(window).load(function () {
+        $("#preloader").fadeOut("slow", function () {
             $(this).remove();
         });
         $(".navbar").css("left", -widthNav);
     });
-    $(window).resize(function(){
-      let resizeW = $(".navbar").innerWidth();
-        $(".navbar").css("left", -resizeW);
+
+  // ReSize Window
+    $(window).resize(function () {
+        let reSize = $(".navbar").innerWidth();
+        $(".navbar").css("left", -reSize);
+        if ($(".navbar").hasClass("show-list")) {
+            closedNavbar();
+        }
     });
-    // Dark Theem
-    $(".box-dark").click(function(e) {
+
+  // Dark Theem
+    $(".box-dark").click(function (e) {
         e.preventDefault();
         $("body").toggleClass("dark-theem");
         $(".dark").toggleClass("dark-toggle");
     });
-    // When Scroll body Sticky Nav bar
-    $(window).scroll(function() {
-        let scroll = $(this).scrollTop(),
+
+  // When Scroll body Sticky Nav bar
+    $(window).scroll(function () {
+        const scroll = $(this).scrollTop(),
             heightHead = $("header").innerHeight();
-        //add class in nav
+    //add class in nav
         if (scroll > 1) {
             if ($("nav").hasClass() !== "top-nav") {
                 $("nav").addClass("top-nav animated fadeInDown");
             }
             if ($(".navbar").hasClass("show-list")) {
-                $(".navbar").removeClass("show-list");
-                $(".navbar").animate(
-                    {
-                        left: -widthNav
-                    },
-                    600
-                );
-                $(".content-nav").animate(
-                    {
-                        width: 0
-                    },
-                    600
-                );
+                closedNavbar();
             }
         } else {
             $("nav").removeClass("top-nav animated fadeInDown");
@@ -56,136 +79,79 @@ $(function() {
         } else {
             $(".top").fadeOut();
         }
-        // add active to scroll
-        $(".block").each(function() {
+    // add active to scroll
+        $(".block").each(function () {
             if ($(window).scrollTop() > $(this).offset().top - 100) {
                 var blockID = $(this).attr("id");
                 $(".nav-link").removeClass("active");
-                $('.nav-link[data-scroll="' + blockID + '"]').addClass(
-                    "active"
-                );
+                $('.nav-link[data-scroll="' + blockID + '"]').addClass("active");
             }
         });
     });
 
-    // Nav Toggle List
-    $(".nav-bar").click(function() {
+  // Nav Toggle List
+    $(".nav-bar").click(function () {
         $(".navbar").toggleClass("show-list");
-        $(".content-nav").animate(
-            {
-                width: 100 + "%"
-            },
-            600
-        );
         if ($(".navbar").hasClass("show-list")) {
-            $(".navbar").animate(
-                {
-                    left: 0
-                },
-                600
-            );
+            showNavbar();
         } else {
-            $(".navbar").animate(
-                {
-                    left: -widthNav
-                },
-                600
-            );
+            closedNavbar();
         }
     });
-    $(".icon, .nav-link ,.content-nav").click(function() {
-        $(".navbar").removeClass("show-list");
-        $(".navbar").animate(
-            {
-                left: -widthNav
-            },
-            600
-        );
+
+  // Closed Navbar
+    $(".icon, .nav-link ,.content-nav").click(function () {
+        closedNavbar();
     });
-    $(".content-nav, .icon ").click(function() {
-        $(".content-nav").animate(
-            {
-                width: 0
-            },
-            600
-        );
-    });
-    // Add Class Active To Nav Linl
-    $(".nav-link").click(function(e) {
-        let navHieght = $("nav").innerHeight();
+  // Add Class Active To Nav Linl
+    $(".nav-link").click(function (e) {
+        const navHieght = $("nav").innerHeight();
         e.preventDefault();
         $(this).addClass("active");
-        $(this)
-            .parent()
-            .siblings()
-            .children()
-            .removeClass("active");
-        $("html, body").animate(
-            {
-                scrollTop:
-                    $("#" + $(this).data("scroll")).offset().top - navHieght
-            },
-            1500
-        );
-        $(".content-nav").animate(
-            {
-                width: 0 + "%"
-            },
-            600
-        );
+        $(this).parent().siblings().children().removeClass("active");
+        $("html, body").animate({
+            scrollTop: $("#" + $(this).data("scroll")).offset().top - navHieght
+        }, 1500);
+        closedNavbar();
     });
-
-    // Set Data To Popup
-    $(".work-link").click(function(e) {
+  // Set Data To Popup
+    $(".work-link").click(function (e) {
         e.preventDefault();
-        const src = $(this)
-            .parent()
-            .children("img")
-            .attr("src");
-        const name = $(this)
-            .parent()
-            .children("h4")
-            .text();
-        const link = $(this)
-            .parent()
-            .attr("dataLink");
-        const skiil = $(this)
-            .parent()
-            .attr("dataSkiils");
+        const src = $(this).parent().children("img").attr("src");
+        const name = $(this).parent().children("h4").text();
+        const link = $(this).parent().attr("dataLink");
+        const skiil = $(this).parent().attr("dataSkiils");
         $(".img-popup").attr("src", src);
         $(".name-pop").text(name);
         $(".link-popup").attr("href", link);
         $(".skiils-pop").text(skiil);
     });
-    // Work Section Btn Shwo Popup
-    $(".work-link").click(function(e) {
+  // Work Section Btn Shwo Popup
+    $(".work-link").click(function (e) {
         e.preventDefault();
         $(".popup-work").fadeIn();
     });
-    // Hide PopUp Fuinctios
-    $(".popup-work").click(function() {
+      // Hide PopUp Fuinctios
+    $(".popup-work").click(function () {
         $(this).fadeOut();
     });
-    $(".content-popup-work").click(function(e) {
+    $(".content-popup-work").click(function (e) {
         e.stopPropagation();
     });
-    $(".popup-work .icon").click(function(e) {
+    $(".popup-work .icon").click(function (e) {
         e.preventDefault();
         $(".popup-work").fadeOut();
     });
-    $(document).keydown(function(e) {
+    $(document).keydown(function (e) {
         if (e.keyCode === 27) {
             $('.popup-work .content-popup-work"').fadeOut();
         }
     });
-    // Goto Top
-    $(".top").click(function(e) {
+      // Goto Top
+    $(".top").click(function (e) {
         e.preventDefault();
-        $("html, body").animate(
-            {
-                scrollTop: 0
-            },
-            1000
-        );
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
     });
 });
